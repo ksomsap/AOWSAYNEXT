87e0e2f68ba20d07fbd8f7fbdafed3f9c283e4cb
KeiP <keittisakcpm@gmail.com>
2019-12-26 23:17:21 +0700

    Next.js app on Heroku

--- server.js
+++ server.js
@@ -1,32 +1,28 @@
-const express = require('express')
-const next = require('next')
+const express = require("express");
+const next = require("next");
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT) || 3000;
+const dev = process.env.NODE_ENV != "production";
+const app = next({dev});
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = express()
+    const server = express();
+    
+    /*server.get("/blog/detail/:id", (req, res) => {
+        return app.render(req, res, "blog/detail", {id: req.params.id});
+    })*/
 
-  server.get('/a', (req, res) => {
-    return app.render(req, res, '/a', req.query)
-  })
-
-  server.get('/b', (req, res) => {
-    return app.render(req, res, '/b', req.query)
-  })
-
-  server.get('/posts/:id', (req, res) => {
-    return app.render(req, res, '/posts', { id: req.params.id })
-  })
-
-  server.all('*', (req, res) => {
-    return handle(req, res)
-  })
-
-  server.listen(port, err => {
+    /*server.get('/blogs/theresume/', (req, res) => {
+        return app.render(req, res, '/blogs/theresume/', req.query)
+      })*/
+    
+    server.get("*", (req,res) => {
+        return handle(req, res);
+    });
+    
+    server.listen(port, err => {
     if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
+        console.log(`> Ready on http://localhost:${port}`)
+    })
 })
