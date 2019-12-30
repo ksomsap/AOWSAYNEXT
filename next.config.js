32e7ea3ce9f3a5271a14120c52d85cb7ca699222
KeiP <keittisakcpm@gmail.com>
2019-12-30 17:11:00 +0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/* next.config.js */
+module.exports = {
+    target: 'serverless', // <- add here
+    webpack: config => {
+      // Fixes npm packages that depend on `fs` module
+      config.node = {
+        fs: 'empty'
+      }
+      return config
+    }
+  }
